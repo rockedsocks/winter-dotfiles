8811189da3ff9b7cffeb5f733cf2f60359994f6c
F F <71783789+rockedsocks@users.noreply.github.com>
2022-04-10 16:56:36 +0200

    Update and rename rlauncher.sh to rofi/launcher.sh

--- rofi/launcher.sh
+++ rofi/launcher.sh
@@ -27,8 +27,6 @@ SELECT="#101010ff"
 #SELECT="#f3f3f3ff"
 
 # accent colors
-COLORS=('#EC7875' '#61C766' '#FDD835' '#42A5F5' '#BA68C8' '#4DD0E1' '#00B19F' \
-		'#FBC02D' '#E57C46' '#AC8476' '#6D8895' '#EC407A' '#B9C244' '#6C77BB')
 ACCENT="#191b42"
 
 # overwrite colors file
